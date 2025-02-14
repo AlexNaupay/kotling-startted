a1522707d2621a0af984800f798f404bf2015e55
Alex from IGP <anaupay@igp.gob.pe>
2025-02-14 09:57:13 -0500

    ...

--- src/main/kotlin/Regex.kt
+++ src/main/kotlin/Regex.kt
@@ -17,6 +17,9 @@ fun main(){
     println("subsJava: $subsJava")
     println("----------------------")
 
+    println("Password: " + isValidPassword("alexhalexh1A<ñAAsefsef"))
+    println("Password punct: " + Pattern.compile("\\p{Punct}+").matcher("ñ").find())
+
 }
 fun isValidName(name: String): Boolean {
     return if (name.isNotEmpty() && name.length < 70) {
@@ -28,7 +31,6 @@ fun isValidName(name: String): Boolean {
     }
 }
 
-
 fun isValidDni(dni: String): Boolean {
     return if (dni.isNotEmpty()) {
         Pattern.compile("[0-9]{8}").matcher(dni).matches()
@@ -39,4 +41,15 @@ fun isValidDni(dni: String): Boolean {
 
 fun isValidNameWithSpaces(name: String): Boolean {
     return "^([A-Z][a-z0-9]*\\s?)+\$".toRegex().matches(name)
+}
+
+fun isValidPassword(password: String): Boolean {
+    return if (password.isNotEmpty() && password.length >= 10) {
+        Pattern.compile("\\d+").matcher(password).find() // Digit
+                && Pattern.compile("[a-z]+").matcher(password).find() // Lower
+                && Pattern.compile("[A-Z]+").matcher(password).find() // Upper
+                && Pattern.compile("\\p{Punct}+").matcher(password).find() // Special
+    } else {
+        false
+    }
 }
